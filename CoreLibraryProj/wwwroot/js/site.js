03234d1cfb0b76e8f37183641ec2e6375f091ac4
Yaroslav <yarik.chushenko1@gmail.com>
2022-03-15 18:48:35 +0200

    Added JS function to change book text language using dropdown menu

--- CoreLibraryProj/wwwroot/js/site.js
+++ CoreLibraryProj/wwwroot/js/site.js
@@ -1,7 +1,24 @@
 ﻿
+
+//Function for Book Index view. 
 function RestoreFilters() {
     document.getElementById("parameter1").value = "";
     document.getElementById("droppar_rubrics").value = "";
     document.getElementById("droppar_authors").value = "";
 
+}
+
+
+//Function for Book Details view.
+function ChangeBookLanguage() {
+
+    //Hiding all language divs
+    for (var i = 0; i < 100; i++) {
+        try { document.getElementById("lan" + i).style.display = "none"; }
+        catch {}
+    }
+
+    //Making the chosen language div visible
+    language_id = document.getElementById("language_front_dropdown").value;
+    document.getElementById("lan" + language_id).style.display = "block";
 }
