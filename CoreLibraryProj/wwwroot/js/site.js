8f9aeee1a20ab2fde36e494dc957e7a3ee78aae5
Yaroslav <yarik.chushenko1@gmail.com>
2022-03-18 17:57:39 +0200

    Added function to change day/night display themes

--- CoreLibraryProj/wwwroot/js/site.js
+++ CoreLibraryProj/wwwroot/js/site.js
@@ -1,4 +1,5 @@
 ﻿
+let current_theme = 'day';
 
 //Function for Book Index view. 
 function RestoreFilters() {
@@ -21,4 +22,36 @@ function ChangeBookLanguage() {
     //Making the chosen language div visible
     language_id = document.getElementById("language_front_dropdown").value;
     document.getElementById("lan" + language_id).style.display = "block";
+}
+
+
+//Function for Book Index View
+function ToggleTheme() {
+
+    if (current_theme == 'day') {
+        document.getElementById('toggle_ball').style.marginLeft = "0px";
+        current_theme = 'night';
+
+
+        var matches = document.querySelectorAll(".search_header, .results_body, .select-css, .search_bar, .results_body_header, h1, .book_desc");
+        matches.forEach(item => item.classList.add("night"));     
+
+
+        document.getElementById('moon').src = "/lib/layout-img/active_moon.svg";
+        document.getElementById('sun').src = "/lib/layout-img/inactive_sun.svg";
+    }
+    else {
+        document.getElementById('toggle_ball').style.marginLeft = "45px";
+        current_theme = 'day';
+        document.getElementById('moon').src = "/lib/layout-img/inactive_moon.svg";
+        document.getElementById('sun').src = "/lib/layout-img/active_sun.svg";
+
+        var matches = document.querySelectorAll(".search_header, .results_body, .select-css, .search_bar, .results_body_header, h1, .book_desc");
+        matches.forEach(item => item.classList.remove("night"));
+
+
+    }
+
+
+   
 }
