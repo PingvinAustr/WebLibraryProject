1fde5f3c1a1b167ab8d020d85c5534573fbb4997
Yaroslav <yarik.chushenko1@gmail.com>
2022-03-23 13:34:45 +0200

    Pagination class

---
+++ CoreLibraryProj/Models/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+
+namespace CoreLibraryProj.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+    }
+}
