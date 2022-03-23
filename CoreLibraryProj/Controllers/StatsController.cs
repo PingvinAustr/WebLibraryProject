529b8135154ecd858ddc8f337852fb4d7f9cefd3
Yaroslav <yarik.chushenko1@gmail.com>
2022-03-23 13:34:25 +0200

    Created blank controller for page with Google Charts API

---
+++ CoreLibraryProj/Controllers/StatsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreLibraryProj.Controllers
+{
+    public class StatsController : Controller
+    {
+        // GET: StatsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
